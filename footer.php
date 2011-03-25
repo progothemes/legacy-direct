f657c60c53a90331219f07339e99c47b481ff624
alex chousmith <alex@ninthlink.com>
2011-03-25 23:21:15 +0000

    quicktweak to add PROGO to footer !

--- footer.php
+++ footer.php
@@ -12,14 +12,16 @@
 ?>
 	</div><!-- #page -->
 	<div id="ftr" class="container_12">
-<?php $fmenu = wp_nav_menu( array( 'container_class' => 'grid_8', 'theme_location' => 'footer', 'echo' => '0' ) );
-$fmenu = str_replace('</li>','&nbsp;&nbsp;|&nbsp;&nbsp;</li>',substr($fmenu,0,strrpos($fmenu,'</li>'))) . "</li>\n</ul>\n</div>\n";
+    <div class="grid_8">
+<?php $fmenu = wp_nav_menu( array( 'container' => 'false', 'theme_location' => 'footer', 'echo' => '0' ) );
+$fmenu = str_replace('</li>','&nbsp;&nbsp;|&nbsp;&nbsp;</li>',substr($fmenu,0,strrpos($fmenu,'</li>'))) . "</li>\n</ul>";
 echo $fmenu;
-?>
-<div class="grid_4 right"><?php
+echo '<br />';
 $options = get_option('progo_options');
 echo wp_kses($options['copyright'],array());
-?></div>
+?>
+</div>
+<div class="grid_4 right">Powered by <a href="http://www.wordpress.org" target="_blank">WordPress</a>. Designed by <a href="http://www.progo.com/" title="Performance WordPress Themes" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/logo_admin.png" alt="ProGo" /></a></div>
 </div><!-- #ftr -->
 </div><!-- #wrap -->
 
